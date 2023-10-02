1eee4bacecfcb687fb8d1ad7cdd20d8a3f161827
Alexander <asperepelov@mail.ru>
2023-10-02 16:29:12 +0300

    add int

--- utils.go
+++ utils.go
@@ -1,8 +1,17 @@
-package Utils //v1.0.1
+package Utils
 
 func Contains(a []string, x string) bool {
 	for _, n := range a {
 		if n == x {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(arr []int, i int) bool {
+	for _, n := range arr {
+		if n == i {
 			return true
 		}
 	}
